8f99007d3e20992eb6ae1d833ef3a2070b8a3b42
leodder <a0919989772@gmail.com>
2026-06-22 21:10:53 +0900

    fix: del /

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,9 +26,9 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   // "https://<你的firebase專案id>.web.app",
-  "https://chat-web-9911f.web.app/",
+  "https://chat-web-9911f.web.app",
   // "https://<你的firebase專案id>.firebaseapp.com",
-  "https://chat-web-9911f.firebaseapp.com/",
+  "https://chat-web-9911f.firebaseapp.com",
 ];
 
 app.use(
